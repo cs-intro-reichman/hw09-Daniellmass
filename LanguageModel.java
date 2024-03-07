322d26c4e7f91bff5554e0b6b81f60b7c3668478
Daniellmass <danielmas221@gmail.com>
2024-03-07 11:50:39 +0200

    updateGenerateMethod

--- LanguageModel.java
+++ LanguageModel.java
@@ -111,11 +111,14 @@ public class LanguageModel {
         while (genText.length() < textLength) {
             String currWindow = genText.substring(Math.max(0, genText.length() - windowLength));
             List charDataL = CharDataMap.get(currWindow);
+            char nextCh;
             if (charDataL == null) {
-                break;
+                nextCh = ' ';
             }
-            char nextCh = getRandomChar(charDataL);
+            else {
+            nextCh = getRandomChar(charDataL);
             genText.append(nextCh);
+            }
         }
         return genText.toString();
 	}
